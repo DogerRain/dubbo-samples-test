421a1f31b302bcbb4863f1338107725145b7ed31
HaC <huangyongwen0306@q63.com>
2021-11-25 22:26:43 +0800

    补充个hessian协议，需要走Tomcat这种Servlet容器，jetty暂时没有测试成功，目前dubbo3.0版本不支持hessian

--- dubbo-samples-xml/dubbo-samples-xml-consumer/src/main/java/com/dubbo/Consumer.java
+++ dubbo-samples-xml/dubbo-samples-xml-consumer/src/main/java/com/dubbo/Consumer.java
@@ -14,15 +14,20 @@ public class Consumer {
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"spring/dubbo-consumer.xml"});
         context.start();
+        System.out.println("consumer start.....");
 
-        OrderService orderService = context.getBean("orderService",OrderService.class);
+        OrderService orderService1 = context.getBean("orderService1",OrderService.class);
+        OrderService orderService2 = context.getBean("orderService2",OrderService.class);
+//        OrderRESTService orderService3 = context.getBean("orderService3",OrderRESTService.class);
 
         while (true) {
             System.in.read();
             RpcContext rpcContext = RpcContext.getContext();
             rpcContext.setAttachment("clientName", "demo");
             rpcContext.setAttachment("clientImpl", "dubbo");
-            System.out.println("SUCCESS: got order " + orderService.getOrderInfo(1L));
+            System.out.println("SUCCESS: got order " + orderService1.getOrderInfo(1L));
+            System.out.println("SUCCESS: got order " + orderService2.getOrderInfo(1L));
+//            System.out.println("SUCCESS: got order " + orderService3.getOrderInfo(1L));
         }
 
     }
