adac236e3f36409b31d6d2eff9d766652bbe79a5
HaC <huangyongwen0306@163.com>
2023-10-20 16:11:26 +0800

    URI create Util

--- dubbo-samples-springboot/dubbo-samples-springboot-provider/src/main/java/com/dubbo/util/EndPointUtils.java
+++ dubbo-samples-springboot/dubbo-samples-springboot-provider/src/main/java/com/dubbo/util/EndPointUtils.java
@@ -1,5 +1,9 @@
 package com.dubbo.util;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
 import java.net.URI;
 
 /**
@@ -10,15 +14,31 @@ import java.net.URI;
 public class EndPointUtils {
     public static void main(String[] args) {
 
-        String endpoint = "http://10.131.1.98:4317";
+        String endpoint = "http://39.156.66.10:443/query";
 
         URI uri = URI.create(endpoint);
 
-        System.out.println(uri.getHost());
+        System.out.println(uri.getHost() + ":" + uri.getPort());
 
+        if (isSocketConnectAlive(uri.getHost(),uri.getPort())){
+            System.out.println("active");
+            int times =  callDeath(0);
+        }
 
-        System.out.println(callDeath(0));
+    }
 
+    public static boolean isSocketConnectAlive(String hostname, int port) {
+        boolean isAlive;
+        SocketAddress socketAddress = new InetSocketAddress(hostname, port);
+        // 超时设置，单位毫秒
+        int timeout = 5000;
+        try (Socket socket = new Socket()) {
+            socket.connect(socketAddress, timeout);
+            isAlive = true;
+        } catch (IOException exception) {
+            isAlive = false;
+        }
+        return isAlive;
     }
 
     public static int callDeath(int count) {
